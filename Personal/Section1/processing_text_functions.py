11ab3709b1c7c0f1ce6e935213de37250944c594
Jordan Shea <jsshea-dev@outlook.com>
2024-04-04 08:56:37 +0100

    Removed stopwords and common words

--- Personal/Section1/processing_text_functions.py
+++ Personal/Section1/processing_text_functions.py
@@ -1,6 +1,8 @@
 '''Functions for processing text'''
 import re
 
+from collections import Counter
+
 
 def improved_regex_splitting(line: str):
     '''Splits a string into individual words'''
@@ -26,11 +28,11 @@ def process_and_split(line: str) -> list:
 def read_and_process(file_path: str,
                      split_lines: bool = False) -> list:
     '''
-    Reads a file and returns a list of each word used in the file
-    
-    Params:
-    file_path: A file path to a text file to process
-    split_lines: If true each line will be returned as an individual list
+        Reads a file and returns a list of each word used in the file
+
+        Params:
+        file_path: A file path to a text file to process
+        split_lines: If true each line will be returned as an individual list
     '''
     processed_corpus = []
     with open(file_path, encoding='UTF-8') as f:
@@ -42,3 +44,31 @@ def read_and_process(file_path: str,
                 else:
                     processed_corpus.extend(line)
     return processed_corpus
+
+
+def remove_uncommon_and_stop_words(
+        corpus: list[str],
+        uncommon_amount: float,
+        stop_words: list[str],) -> list[str]:
+    '''
+        Removes uncommon words and stop words from a corpus
+
+        Params:
+        corpus: A list of strings to be processed
+        uncommon_amount: If 1 or above will be a number of words to
+            be removed. If below 1, then will be a proportion to be
+            removed.
+        stop_words: A list of common words without meaning to remove
+
+        Returns a list of words from the corpus
+    '''
+    # Find least common elements
+    word_counts = Counter(corpus)
+    if uncommon_amount > 1:
+        uncommon_words = word_counts.most_common()[:-(uncommon_amount+1):-1]
+    else:
+        removal_amount = int(uncommon_amount*len(corpus))
+        uncommon_words = word_counts.most_common()[:-(removal_amount+1):-1]
+
+    reduced_corpus = [w for w in corpus if w not in stop_words]
+    return [w for w in reduced_corpus if w not in uncommon_words]
